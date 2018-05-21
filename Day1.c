bd5414ad742aec767c481780ac9889fa6c7877a5
bcarlson01 <bcarlson@berkeley.edu>
2018-05-21 02:14:36 -0700

    Learning about printf

--- Day1.c
+++ Day1.c
@@ -1,123 +1,113 @@
+/* 
+ * Tells the compiler to include contents of stdio.h
+ * Contains basic functions such as scanf() and printf()
+ */
 #include <stdio.h>
-#include <string.h>
 
-int hello_world();
-int ask_int();
-void multi_program();
-void palindrome();
-int is_palindrome(char *str);
-void inplace_reverse(char *str);
+/*
+ * Must declare/define methods before they are called
+ */
+// int methods return type int
+int int_square(int x) {
+    return x * x;
+}
+// float methdos return type float
+float float_square(float x) {
+    return x * x;
+}
+// void methods return NULL
+void printf_examples();
 
+/*
+ * The execution of a C program begins from the main() function
+ */
 int main() {
-    multi_program();
-}
-
-int hello_world() {
-    // printf() displays the formatted output
-    printf("Hello World\n");
+    printf_examples();
+    // Exit statement of the program - where it ends
     return 0;
-}
+}  
 
-int ask_int() {
-    int number;
-    printf("Enter an integer: ");  
-    
-    // scanf() reads the formatted input and stores them
-    scanf("%d", &number);  
-
-    printf("You entered: %d\n", number);
-    return 0;
-}
-
-void multi_program() {
-    int loop = 0;
-    char s1[2];
-
-    printf("Do you want to loop the program? (y/n): ");
-    fgets(s1, sizeof(s1), stdin);
-
-    while (*s1 != 'y' || *s1 != 'n') {
-        printf("That is not a valid input.\n");
-        printf("Please enter either y or n.\n");
-        printf("Do you want to loop the program? (y/n): ");
-        fgets(s1, sizeof(s1), stdin);
-    }
-    if (*s1 == 'y') {
-        loop = 1;
-    } else {
-        palindrome();
-    }
-
-    while (loop) {
-        palindrome();
-        printf("Continue? (y/n): ");
-        fgets(s1, sizeof(s1), stdin);
-        while (*s1 != 'y' || *s1 != 'n') {
-            printf("That is not a valid input.\n");
-            printf("Please enter either y or n.\n");
-            printf("Continue? (y/n): ");
-            fgets(s1, sizeof(s1), stdin);
-        }
-        if (*s1 == 'n') {
-            loop = 0;
-        }
-    }
-}
-
-void palindrome() {
-    char s2[1000];
-    printf("Enter Palindrome: ");
-    fgets(s2, sizeof(s2), stdin);
-    s2[999] = '\0';
-    if (is_palindrome(s2)) {
-        printf("This is a palindrome!");
-    } else {
-        printf("This is not a palindrome. ):<");
-    }
-}
-
-int is_palindrome(char *str) {
-    if (str) {
-        char * end = str + strlen(str) - 1;
-
-        while (str < end) {
-            if (*str != *end) {
-                return 0;
-            }
-            str++;
-            end--;
-        }   
-    }
-    return 1;
-}
-
-void inplace_reverse(char *str) {
+/*
+ * A method with many printf() examples
+ */
+void printf_examples() {
     /*
-     * Found on StackOverflow 
-     * https://stackoverflow.com/questions/784417/reversing-a-string-in-c
-     * By Username: rampion
-     * Reverses the given null-terminated string in place
+     * Format is: %[flags][width][.precision][length]specifier
      */
-    if (str) {
-        char * end = str + strlen(str) - 1;
+    /*
+     * Specifiers:
+     * %c - character
+     * %d, %i - signed decimal integer
+     * %e - scientific notation (mantissa/exponent) using e
+     * %E - scientific notation (mantissa/exponent) using E
+     * %f - decimal floating point number
+     * %g - uses shorter of %e or %f
+     * %G - uses shorter of %E or %f
+     * %o - signed octal number
+     * %s - string of characters
+     * %u - unsigned decimal integers
+     * %x - unsigned hexadecimal integers
+     * %X - unsigned hexadecimal integers, with capital letters
+     * %p - pointer address
+     * %n - nothing printed
+     * %% - %
+     */
+    /* 
+     * flags:
+     * - - left-justify (default right-justify)
+     * + - precede the result with a +/- sign (default only - has sign)
+     * (space) - insert a space before the value if no sign
+     * # - used with %o, %x, %X, the value is preceded by 0, 0x, 0X
+     *      respectively for values different than 0
+     *     used with %e, %E, %f, %g, %G, forces written output to contain
+     *      a decimal point even if no digits would follow (by default, 
+     *      if no digits follow, no decimal point is written)
+     * 0 - left-pads the number with 0's instead of spaces
+     */
+    /*
+     * width:
+     * (number) - minimum number of characters to be printed
+     *      if a larger number of characters, none are truncated 
+     * * - the width is not specified in the format string, but as an
+     *      additional integer value argument preceding the argument to be
+     *      formatted
+     */
+    /*
+     * .precision:
+     * .number - specifies minimum number of digits to be written.
+     *      if value is shorter, padded with leading zeros
+     * .* - precision is not specified in the format string, but as an 
+     *      additional integer value argument preceding the argument to be 
+     *      formatted
+     */
+    /*
+     * length:
+     * h - argument is interpretted as a short int or an unsigned short int
+     * l - argument is interpretted as a long int or an unsigned long int for
+     *      integer specifiers, and as a wide character or wide character
+     *      string for %c and %s
+     * L - argument is interpretted as a long double
+     */
+    /*
+     * Additional arguments should match the number of % specifiers in the
+     *  string to be printed, and each additional argument is respective to
+     *  a % specifier
+     * 
+     * Return value: number of characters written, or a negative value if fail
+     */
+    //Examples
+    char c1 = 'X';
+    char* s1 = "I am a string";
+    printf("Here is a char: %c; Here is a string: %s\n", c1, s1);
 
-        // swap the values in the two given variables
-        // XXX: fails when a and b refer to same memory location
-        #define XOR_SWAP(a,b) do\
-        {\
-            a ^= b;\
-            b ^= a;\
-            a ^= b;\
-        } while (0)
 
-        // walk inwards from both ends of the string, 
-        // swapping until we get to the middle
-        while (str < end)
-        {
-            XOR_SWAP(*str, *end);
-            str++;
-            end--;
-        }
-        #undef XOR_SWAP
-    }
 }
+
+
+
+
+
+
+
+
+
